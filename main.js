af4b3bcd78728f8689a52d6cd32667c52c716723
freddiecabrera <freddiecabrera18@yahoo.com>
2016-02-26 13:59:43 -0800

    receive input from keyboard

--- main.js
+++ main.js
@@ -2,22 +2,38 @@
 'use strict';
 var $display;
 $(document).ready(init);
+
 function init(){
   $display = $('#display');
+
+  $(document).on('keypress', keyPressed);
+
   $('.num').click(clickNum);
   $('#clear').click(clearClicked);
-
-
-
-
 }
 
 
 function clickNum() {
-  var $this = $(this);
-  console.log($this.text());
+  var num = $(this).text();
+  addNumToDisplay(num);
 }
 
 function clearClicked(){
   $('#display').text(0);
+}
+
+function addNumToDisplay(num) {
+  var currentDisplay = $display.text();
+
+  if(currentDisplay === '0'){
+    $display.text(num);
+  } else{
+    $display.text(currentDisplay + num);
+  }
+}
+
+function keyPressed(event) {
+  event.preventDefault();
+  var key = String.fromCharCode(event.charCode);
+  console.log(key);
 }
