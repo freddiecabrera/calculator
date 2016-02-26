249b1d05df243c90b21649e263bafc3fd399579c
freddiecabrera <freddiecabrera18@yahoo.com>
2016-02-26 12:12:48 -0800

    add calculator add functionality to AC

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 
+'use strict';
+var $display;
+$(document).ready(init);
+function init(){
+  $display = $('#display');
+  $('.num').click(clickNum);
+  $('#clear').click(clearClicked);
+
+
+
+
+}
+
+
+function clickNum() {
+  var $this = $(this);
+  console.log($this.text());
+}
+
+function clearClicked(){
+  $('#display').text(0);
+}
